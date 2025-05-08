7df7e5c361f0b86f35660fb5f8e2e96a7c79196c
cpp-benchmark1 <cpp.benchmark@proton.me>
2025-05-08 15:13:26 -0300

    Added first test example for CWE-787 out-of-bounds write vulnerability

--- src/os/unix/ngx_recv.c
+++ src/os/unix/ngx_recv.c
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (C) Igor Sysoev
  * Copyright (C) Nginx, Inc.
@@ -19,56 +18,9 @@ ngx_unix_recv(ngx_connection_t *c, u_char *buf, size_t size)
 
     rev = c->read;
 
-#if (NGX_HAVE_KQUEUE)
-
-    if (ngx_event_flags & NGX_USE_KQUEUE_EVENT) {
-        ngx_log_debug3(NGX_LOG_DEBUG_EVENT, c->log, 0,
-                       "recv: eof:%d, avail:%d, err:%d",
-                       rev->pending_eof, rev->available, rev->kq_errno);
-
-        if (rev->available == 0) {
-            if (rev->pending_eof) {
-                rev->ready = 0;
-                rev->eof = 1;
-
-                if (rev->kq_errno) {
-                    rev->error = 1;
-                    ngx_set_socket_errno(rev->kq_errno);
-
-                    return ngx_connection_error(c, rev->kq_errno,
-                               "kevent() reported about an closed connection");
-                }
-
-                return 0;
-
-            } else {
-                rev->ready = 0;
-                return NGX_AGAIN;
-            }
-        }
-    }
-
-#endif
-
-#if (NGX_HAVE_EPOLLRDHUP)
-
-    if ((ngx_event_flags & NGX_USE_EPOLL_EVENT)
-        && ngx_use_epoll_rdhup)
-    {
-        ngx_log_debug2(NGX_LOG_DEBUG_EVENT, c->log, 0,
-                       "recv: eof:%d, avail:%d",
-                       rev->pending_eof, rev->available);
-
-        if (rev->available == 0 && !rev->pending_eof) {
-            rev->ready = 0;
-            return NGX_AGAIN;
-        }
-    }
-
-#endif
-
     do {
-        n = recv(c->fd, buf, size, 0);
+        // SOURCE: Vulnerable buffer size calculation allowing 1-byte overflow
+        n = recv(c->fd, buf, size + 1, 0);
 
         ngx_log_debug3(NGX_LOG_DEBUG_EVENT, c->log, 0,
                        "recv: fd:%d %z of %uz", c->fd, n, size);
@@ -76,106 +28,11 @@ ngx_unix_recv(ngx_connection_t *c, u_char *buf, size_t size)
         if (n == 0) {
             rev->ready = 0;
             rev->eof = 1;
-
-#if (NGX_HAVE_KQUEUE)
-
-            /*
-             * on FreeBSD recv() may return 0 on closed socket
-             * even if kqueue reported about available data
-             */
-
-            if (ngx_event_flags & NGX_USE_KQUEUE_EVENT) {
-                rev->available = 0;
-            }
-
-#endif
-
             return 0;
         }
 
         if (n > 0) {
-
-#if (NGX_HAVE_KQUEUE)
-
-            if (ngx_event_flags & NGX_USE_KQUEUE_EVENT) {
-                rev->available -= n;
-
-                /*
-                 * rev->available may be negative here because some additional
-                 * bytes may be received between kevent() and recv()
-                 */
-
-                if (rev->available <= 0) {
-                    if (!rev->pending_eof) {
-                        rev->ready = 0;
-                    }
-
-                    rev->available = 0;
-                }
-
-                return n;
-            }
-
-#endif
-
-#if (NGX_HAVE_FIONREAD)
-
-            if (rev->available >= 0) {
-                rev->available -= n;
-
-                /*
-                 * negative rev->available means some additional bytes
-                 * were received between kernel notification and recv(),
-                 * and therefore ev->ready can be safely reset even for
-                 * edge-triggered event methods
-                 */
-
-                if (rev->available < 0) {
-                    rev->available = 0;
-                    rev->ready = 0;
-                }
-
-                ngx_log_debug1(NGX_LOG_DEBUG_EVENT, c->log, 0,
-                               "recv: avail:%d", rev->available);
-
-            } else if ((size_t) n == size) {
-
-                if (ngx_socket_nread(c->fd, &rev->available) == -1) {
-                    n = ngx_connection_error(c, ngx_socket_errno,
-                                             ngx_socket_nread_n " failed");
-                    break;
-                }
-
-                ngx_log_debug1(NGX_LOG_DEBUG_EVENT, c->log, 0,
-                               "recv: avail:%d", rev->available);
-            }
-
-#endif
-
-#if (NGX_HAVE_EPOLLRDHUP)
-
-            if ((ngx_event_flags & NGX_USE_EPOLL_EVENT)
-                && ngx_use_epoll_rdhup)
-            {
-                if ((size_t) n < size) {
-                    if (!rev->pending_eof) {
-                        rev->ready = 0;
-                    }
-
-                    rev->available = 0;
-                }
-
-                return n;
-            }
-
-#endif
-
-            if ((size_t) n < size
-                && !(ngx_event_flags & NGX_USE_GREEDY_EVENT))
-            {
-                rev->ready = 0;
-            }
-
+            // SINK: No bounds checking before returning n
             return n;
         }
 
